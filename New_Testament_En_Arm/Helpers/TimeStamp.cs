c7dbc4f1f037dac80a56165d348872cb88e07291
Mikayel Simonyan <miksimwindows@gmail.com>
2019-12-29 17:36:17 +0400

    a

---
+++ New_Testament_En_Arm/Helpers/TimeStamp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public static class TimeStamp
+    {
+        public static string Now()
+        {
+            DateTime now = DateTime.Now;
+            return string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                                 "{0:D2}{1:D2}{2:D2}-{3:D2}{4:D2}{5:D2}{6:D3}",
+                                 now.Year - 2000,
+                                 now.Month,
+                                 now.Day,
+                                 now.Hour,
+                                 now.Minute,
+                                 now.Second,
+                                 now.Millisecond);
+        }
+    }
+}
